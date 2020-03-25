15bf3a5cf13ec0d5811fcbfd37fc8e3db3a587a5
Hika39 <62626291+Hika39@users.noreply.github.com>
2020-03-25 15:06:41 +0100

    Add files via upload
    Prima versione della tendina, devo ancora sistemare gli <a> e metterli in una <ul> per poter fare la lista

---
+++ app.js
@@ -0,0 +1,95 @@
+
+/* BURGER */
+var y=document.querySelector("#menu2");
+var x=document.querySelector("#menu3");
+var z=document.querySelector("#menu1");
+var k=document.querySelector("#tendina");
+
+var c=0;
+var i=0;
+
+var linkTendina = document.querySelectorAll('.link-tendina');
+var sottoLinkTendina = document.querySelectorAll('.sottolink-tendina');
+console.log(linkTendina);
+console.log(sottoLinkTendina);
+
+var frecciaTendina1 = document.querySelectorAll('.freccia-tendina1 path');
+var frecciaTendina2 = document.querySelectorAll('.freccia-tendina2 path');
+
+function fburger(){
+	if(c%2===0){
+   		y.style.transform="rotate(45deg)";
+   		x.style.transform="rotate(-45deg)";
+   		z.style.opacity=0;
+   		x.style.backgroundColor="#E21B23";
+   		y.style.backgroundColor="#E21B23";
+   		z.style.backgroundColor="#E21B23";
+   		k.style.left="0vw";
+
+   		for(i=0;i<linkTendina.length;i++) {			//animazione per le scritte all'interno della tendina all'apertura
+			linkTendina[i].style.opacity = "1";
+			linkTendina[i].style.transform = "translateX(0)";
+		}
+
+		for(i=0;i<sottoLinkTendina.length;i++) {			//animazione per le sottoscritte all'interno della tendina all'apertura
+			sottoLinkTendina[i].style.opacity = "1";
+			sottoLinkTendina[i].style.transform = "translateY(0)";
+		}
+
+
+
+		// anime({											//animazione freccie1 della tendina all'apertura
+  // 			targets: frecciaTendina1,
+  // 			opacity: 1,
+  // 			strokeDashoffset: [anime.setDashoffset, 0],
+  // 			easing: 'easeInOutSine',
+  // 			duration: 500,
+  // 			delay: function(el, i) { return i * 250 }
+		// });
+
+		// anime({											//animazione freccie2 della tendina all'apertura
+  // 			targets: frecciaTendina2,
+  // 			opacity: 1,
+  // 			strokeDashoffset: [anime.setDashoffset, 0],
+  // 			easing: 'easeInOutSine',
+  // 			duration: 500,
+  // 			delay: function(el, i) { return i * 250 }
+		// });
+
+
+	}
+
+	else{
+		y.style.transform="rotate(0deg)";
+		x.style.transform="rotate(0deg)";
+		z.style.opacity=1;
+		x.style.transform="translate3d(0vh, 1.2vh, 0vh)";
+		y.style.transform="translate3d(0vh, -1.2vh, 0vh)";
+		x.style.backgroundColor="white";
+		y.style.backgroundColor="white";
+		z.style.backgroundColor="white";
+		x.style="hover #E21B23";
+		y.style="hover #E21B23";
+		z.style="hover #E21B23";
+		k.style.left="-20vw";
+
+		for(i=0;i<linkTendina.length;i++) {			//animazione per le scritte all'interno della tendina alla chiusura
+			linkTendina[i].style.opacity = "0";
+			linkTendina[i].style.transform = "translateX(-5vw)"
+		}
+
+		for(i=0;i<sottoLinkTendina.length;i++) {			//animazione per le sottoscritte all'interno della tendina all'apertura
+			sottoLinkTendina[i].style.opacity = "1";
+			sottoLinkTendina[i].style.transform = "translateY(-5vh)";
+		}
+
+	}
+	c++;
+
+}
+
+ /* scroll */
+function fscroll(){
+	document.querySelector(".html").style.scrollBehavior="smooth";
+}
+
